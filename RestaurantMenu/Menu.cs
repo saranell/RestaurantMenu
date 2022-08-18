f689609c536fc29547dbeaaf6ba31df15bcaf667
saranell <76405224+saranell@users.noreply.github.com>
2022-08-17 21:05:17 -0500

    First commit

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public List<MenuItem> Items { get; set; }
+        public string DateTime { get; set; }
+
+        public Menu(List<MenuItem> items, string dateTime)
+        {
+            Items = items;
+            DateTime = dateTime;
+        }
+    }
+}
+
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+
+        public MenuItem(string itemName, double v, string description, string category, bool isNew)
+        {
+            ItemName = itemName;
+            Description = description;
+            Category = category;
+            IsNew = IsNew;
+
+        }
+    }
+}
+
